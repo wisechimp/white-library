71274e1bab0990352dfd7739d82e51e9a7cbc206
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2026-07-04 14:39:53 +0200

    feat: Updating configuration to accommodate the  WD issue posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,19 @@
-import { defineCollection } from 'astro:content';
-import { glob } from 'astro/loaders';
-import { z } from 'astro/zod';
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
-			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
-		}),
+const issues = defineCollection({
+  // Load Markdown and MDX files in the `src/content/issues/` directory.
+  loader: glob({ base: "./src/content/issues", pattern: "**/[^_]*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      issue: z.string(),
+      summary: z.string(),
+      // Transform string to Date object
+      date: z.string(),
+      coverSrc: z.optional(image()),
+    }),
 });
 
-export const collections = { blog };
+export const collections = { issues };
