e37984f8f27ce40aabfa79d53f696d7e6e2df3e3
Stuart Hicks <17533346+wisechimp@users.noreply.github.com>
2026-07-04 17:54:10 +0200

    feat: Updating the rss file for issues
    Removing references to posts

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const posts = await getCollection("issues");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: issues.map((issue) => ({
+      ...issue.data,
+      link: `/issues/white-dwarf${issue.data.issue}/`,
+    })),
+  });
 }
